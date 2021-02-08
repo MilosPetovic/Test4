4f7f689ca1f293c05477e5c816b4f38004cdb1b9
MilosPetovic <75281926+MilosPetovic@users.noreply.github.com>
2021-02-08 13:33:57 +0100

    Update Select.js

--- src/comp/Select.js
+++ src/comp/Select.js
@@ -1,13 +1,15 @@
 const Select = ({launches,setLaunches,setSelect,}) => {
 
-    // const some = Array(launches.map(el => launches.slice(0,21)))
+    // const some =launches.slice(0,21)
 
     return (
         <div>
             <select onChange={(e) => {
                 setSelect(e.target.value)
             }}>
-
+            <options>20</options>
+            <options>30</options>
+            <options>50</options>
             </select>
         </div>
         )
